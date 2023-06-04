66e34ad14929d0db2ce43320f84e19a9fc659579
vuongkienthanh <thanhstardust@outlook.com>
2023-06-04 15:09:05 +0700

    change start dist

--- build_app_windows.bat
+++ build_app_windows.bat
@@ -1,3 +1,3 @@
 set PYTHONOPTIMIZE=2
 poetry run pyinstaller SimpleClinic.spec
-start build
+start dist
--- build_demo_windows.bat
+++ build_demo_windows.bat
@@ -1,3 +1,3 @@
 set PYTHONOPTIMIZE=2
 poetry run pyinstaller src\demo.py --onefile --add-data="src\bitmaps;bitmaps" --add-data="src\default_config.json;." --add-data="src\sample;sample" -i="logo\logo.ico"
-start build
+start dist
